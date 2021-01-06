a87a8e717253c7d6207bdebd12f180a8b7f38b00
Joel Rebello <jrebello@packet.com>
2021-01-06 15:47:24 +0100

    supermicro: implement interface methods

--- providers/supermicro/supermicro.go
+++ providers/supermicro/supermicro.go
@@ -8,180 +8,92 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-//import (
-//	"encoding/json"
-//	"fmt"
-//
-//	"github.com/packethost/fup/apiclient"
-//	"github.com/packethost/fup/internal/device/utils"
-//	"github.com/packethost/fup/internal/model"
-//	"github.com/sirupsen/logrus"
-//)
-//
-//// For the future Joel, it may be worth parsing the inventory catalog
-//// /usr/libexec/dell_dup/inv.xml
-//
 type Supermicro struct {
-	requiresReboot   bool // set when the device requires a reboot after update
-	UpdatesAvailable int
-	Vendor           string
-	Model            string
-	Serial           string
-	Components       []*model.Component
-	Collectors       map[string]utils.Collector
-	Logger           *logrus.Logger
-	Dmidecode        *utils.Dmidecode
+	ID                   string
+	PendingReboot        bool // set when the device requires a reboot after update
+	UpdatesAvailable     int
+	Vendor               string
+	Model                string
+	Serial               string
+	Components           []*model.Component
+	Collectors           map[string]utils.Collector
+	Logger               *logrus.Logger
+	Dmidecode            *utils.Dmidecode
+	FirmwareUpdateConfig *model.FirmwareUpdateConfig
 }
 
-func (s *Supermicro) GetInventory(ctx context.Context) (*model.Device, error) {
-	return nil, nil
+func (s *Supermicro) GetModel() string {
+	return s.Model
 }
 
-//
-//type Component struct {
-//	Serial   string
-//	Type     string
-//	Model    string
-//	Firmware string
-//}
-//
-//func New(dmi *utils.Dmidecode, vendor, model string, api *apiclient.Client, l *logrus.Logger) *Supermicro {
-//
-//	var trace bool
-//
-//	if l.GetLevel().String() == "trace" {
-//		trace = true
-//	}
-//
-//	collectors := map[string]utils.Collector{
-//		"ipmi":     utils.NewIpmicfgCmd(trace),
-//		"smartctl": utils.NewSmartctlCmd(trace),
-//		"storecli": utils.NewStoreCLICmd(trace),
-//		"mlxup":    utils.NewMlxupCmd(trace),
-//	}
-//
-//	return &Supermicro{
-//		api:        api,
-//		dmi:        dmi,
-//		collectors: collectors,
-//		logger:     l,
-//		Model:      model,
-//		Vendor:     "supermicro",
-//	}
-//}
-//
-//func (s *Supermicro) ValidateUpdatesInstalled() (err error) {
-//
-//	err = s.CollectFirmwareInventory()
-//	if err != nil {
-//		return err
-//	}
-//
-//	if s.UpdatesAvailable > 0 {
-//		s.logger.WithField("updates", s.UpdatesAvailable).Fatal("All Updates for device were not applied, or newer updates available ", model.ClientFirmareInventoryLog)
-//	}
-//
-//	return nil
-//}
-//
-//// Collect firmware inventory and push to fup API
-//func (s *Supermicro) CollectFirmwareInventory() error {
-//
-//	inventory := make([]*model.FirmwareInventory, 0)
-//	// collect current component firmware versions
-//	s.logger.Info("Identifying component firmware versions...")
-//	for cmd, collector := range s.collectors {
-//		inv, err := collector.DeviceAttributes()
-//		if err != nil {
-//			s.logger.WithFields(logrus.Fields{"cmd": cmd, "err": err}).Error("inventory collector error")
-//		}
-//
-//		if len(inv) == 0 {
-//			s.logger.WithFields(logrus.Fields{"cmd": cmd}).Trace("inventory collector returned no items")
-//			continue
-//		}
-//
-//		inventory = append(inventory, inv...)
-//	}
-//
-//	s.FirmwareInventory = inventory
-//	err := s.api.InsertFirmwareInventory(inventory)
-//	if err != nil {
-//		return err
-//	}
-//	s.logger.Info("Component firmware inventory sent to fup")
-//
-//	return err
-//}
-//
-//func (s *Supermicro) ListFirmwareInventory() (err error) {
-//
-//	inventory := make([]*model.FirmwareInventory, 0)
-//	// collect current component firmware versions
-//	s.logger.Info("Identifying component firmware versions...")
-//	for cmd, collector := range s.collectors {
-//		inv, err := collector.DeviceAttributes()
-//		if err != nil {
-//			s.logger.WithFields(logrus.Fields{"cmd": cmd, "err": err}).Error("inventory collector error")
-//		}
-//
-//		if len(inv) == 0 {
-//			s.logger.WithFields(logrus.Fields{"cmd": cmd}).Trace("inventory collector returned no items")
-//			continue
-//		}
-//		inventory = append(inventory, inv...)
-//	}
-//
-//	b, _ := json.MarshalIndent(inventory, "", " ")
-//	fmt.Print(string(b))
-//	return nil
-//}
-//
-//func (s *Supermicro) ApplyUpdate() (err error) {
-//	return err
-//}
-//
-//func (s *Supermicro) BIOS() (err error) {
-//	return err
-//}
-//
-//func (s *Supermicro) BMC() (err error) {
-//	return err
-//}
-//
-//func (s *Supermicro) SetDeviceID(id string) {
-//	s.DeviceID = id
-//}
-//
-//func (s *Supermicro) SetLocation(l string) {
-//	s.Location = l
-//}
-//
-//func (s *Supermicro) SetFirmwareManifest(m *model.FirmwareConfig) {
-//	s.FirmwareManifest = m
-//}
-//
-//func (s *Supermicro) SetDeployManifest(m *model.Deployment) {
-//	s.DeployManifest = m
-//}
-//
-//func (s *Supermicro) GetDeviceID() string {
-//	return s.DeviceID
-//}
-//
-//func (s *Supermicro) GetLocation() string {
-//	return s.Location
-//}
-//
-//func (s *Supermicro) GetModel() string {
-//	return s.Model
-//}
-//
-//func (s *Supermicro) GetVendor() string {
-//	return s.Vendor
-//}
-//
-//func (s *Supermicro) RequiresReboot() bool {
-//	return s.requiresReboot
-//}
-//
+func (s *Supermicro) GetVendor() string {
+	return s.Vendor
+}
+
+func (s *Supermicro) GetDeviceID() string {
+	return s.ID
+}
+
+func (s *Supermicro) SetDeviceID(id string) {
+	s.ID = id
+}
+
+func (s *Supermicro) RebootRequired() bool {
+	return s.PendingReboot
+}
+
+func (s *Supermicro) SetFirmwareUpdateConfig(config *model.FirmwareUpdateConfig) {
+	s.FirmwareUpdateConfig = config
+}
+
+func (s *Supermicro) SetOptions(options map[string]interface{}) error {
+	return nil
+}
+
+// Returns hardware inventory for the device
+func (s *Supermicro) GetInventory(ctx context.Context, listUpdates bool) (*model.Device, error) {
+
+	inventory := make([]*model.Component, 0)
+	// collect current component firmware versions
+	s.Logger.Info("Identifying component firmware versions...")
+	for cmd, collector := range s.Collectors {
+		inv, err := collector.Components()
+		if err != nil {
+			s.Logger.WithFields(logrus.Fields{"cmd": cmd, "err": err}).Error("inventory collector error")
+		}
+
+		if len(inv) == 0 {
+			s.Logger.WithFields(logrus.Fields{"cmd": cmd}).Trace("inventory collector returned no items")
+			continue
+		}
+
+		inventory = append(inventory, inv...)
+	}
+
+	s.Components = inventory
+	device := &model.Device{
+		ID:         s.ID,
+		Serial:     s.Serial,
+		Model:      s.Model,
+		Vendor:     s.Vendor,
+		Oem:        true,
+		Components: s.Components,
+	}
+
+	return device, nil
+}
+
+func (s *Supermicro) GetUpdatesAvailable(ctx context.Context) (*model.Device, error) {
+	return &model.Device{}, nil
+}
+
+// TODO: decide how we calculate the device firmware revision
+//  one possibility is to SHA the firmware versions of all components
+func (s *Supermicro) GetDeviceFirmwareRevision(ctx context.Context) (string, error) {
+	return "", nil
+}
+
+func (s *Supermicro) ApplyUpdatesAvailable(ctx context.Context) (err error) {
+
+	return nil
+}
