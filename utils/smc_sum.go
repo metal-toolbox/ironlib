c4c238977fe69cb8b7f0e3c453c46d781fda1ff8
Joel Rebello <jrebello@packet.com>
2021-01-12 15:23:15 +0100

    supermicro: implement bmc, bios update methods

---
+++ utils/smc_sum.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+)
+
+const smcSUM = "/usr/sbin/sum"
+
+type SupermicroSUM struct {
+	Executor Executor
+}
+
+// Return a new Supermicro sum command executor
+func NewSupermicroSUM(trace bool) Updater {
+
+	e := NewExecutor(smcSUM)
+	e.SetEnv([]string{"LC_ALL=C.UTF-8"})
+	if !trace {
+		e.SetQuiet()
+	}
+
+	return &SupermicroSUM{Executor: e}
+}
+
+// Fake Supermicro SUM executor for tests
+func NewFakeSupermicroSUM() *SupermicroSUM {
+	return &SupermicroSUM{
+		Executor: NewFakeExecutor("sum"),
+	}
+}
+
+func (s *SupermicroSUM) ApplyUpdate(ctx context.Context, updateFile, componentSlug string) error {
+
+	switch componentSlug {
+	case "bios":
+		s.Executor.SetArgs([]string{"-c", "UpdateBios", "--preserve_setting", "--file", updateFile})
+	case "bmc":
+		s.Executor.SetArgs([]string{"-c", "UpdateBmc", "--file", updateFile})
+	}
+
+	result, err := s.Executor.ExecWithContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	if result.ExitCode != 0 {
+		return fmt.Errorf("%s returned non-zero exit code: %d, stderr: %s", s.Executor.GetCmd(), result.ExitCode, result.Stderr)
+	}
+
+	return nil
+}
