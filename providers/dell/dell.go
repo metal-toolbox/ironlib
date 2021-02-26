afe67581f27efd13755102334c8062bce3ec26f2
Joel Rebello <jrebello@packet.com>
2021-02-26 10:58:29 +0100

    dell: refactor ApplyUpdatesAvailable
    - move out helper methods
    - add various checks, flags

--- providers/dell/dell.go
+++ providers/dell/dell.go
@@ -170,190 +170,41 @@ func (d *Dell) GetDeviceFirmwareRevision(ctx context.Context) (string, error) {
 	return d.Dsu.Version()
 }
 
-// sets d.ComponentUpdates to the slice of updates identified by the dell-system-update utility
-func (d *Dell) listUpdatesAvailable() error {
-
-	err := d.pre()
-	if err != nil {
-		return err
-	}
-
-	// collect firmware updates available for components
-	componentUpdates, exitCode, err := d.Dsu.ComponentFirmwareUpdatePreview()
-	if err != nil && exitCode != utils.DSUExitCodeNoUpdatesAvailable {
-		return err
-	}
-
-	d.ComponentUpdates = componentUpdates
-
-	return nil
-}
-
 // nolint: gocyclo
+// Installs either all updates identified by DSU, OR component level updates if any listed
 func (d *Dell) ApplyUpdatesAvailable(ctx context.Context, config *model.FirmwareUpdateConfig, dryRun bool) (err error) {
 
 	if config == nil {
 		return fmt.Errorf("ApplyUpdatesAvailable() requires a valid *model.FirmwareUpdateConfig")
 	}
 
-	d.FirmwareUpdateConfig = config
-
-	// collect data before we proceed to apply updates
-	steps := []func() error{d.pre}
-
-	// lookup component updates if it wasn't done earlier
-	if len(d.ComponentUpdates) == 0 {
-		steps = append(steps, d.listUpdatesAvailable)
+	// atleast one of these is required
+	if len(config.Updates) == 0 && len(config.Components) == 0 {
+		return fmt.Errorf("expected a valid 'updates:' and/or 'components:' list in the firmware config")
 	}
 
-	for _, step := range steps {
-		err = step()
+	d.DM.FirmwareUpdateConfig = config
+
+	// fetch firmware inventory if updates were not yet identified
+	if d.DM.UpdatesAvailable == -1 {
+		_, err = d.GetInventory(ctx, true)
 		if err != nil {
 			return err
 		}
 	}
 
-	if len(d.ComponentUpdates) == 0 {
-		d.Logger.Info("No updates to be applied, all components are up to date as per firmware configuration")
-		return nil
+	if len(d.DM.Device.ComponentUpdates) == 0 {
+		d.DM.PendingReboot = false
+		d.DM.UpdatesInstalled = false
+		d.DM.UpdatesAvailable = 0
+		return errs.ErrNoUpdatesApplicable
 	}
 
-	for _, component := range d.ComponentUpdates {
-		d.Logger.WithFields(logrus.Fields{"slug": component.Slug, "name": component.Name, "installed": component.FirmwareInstalled, "available": component.FirmwareAvailable}).Info("component update to be applied")
-	}
-
-	if dryRun {
-		return nil
-	}
-
-	// log update process to stdout
-	d.Dsu.Executor.SetVerbose()
-
-	// apply updates
-	exitCode, err := d.Dsu.ApplyUpdates()
-
-	d.Dsu.Executor.SetQuiet()
-
-	// check exit code - see dsu_return_codes.md
-	switch exitCode {
-	case utils.DSUExitCodeUpdatesApplied:
-		d.UpdatesInstalled = true
-		d.Logger.Infoln("updates applied successfully")
-		return nil
-	case utils.DSUExitCodeNoUpdatesAvailable: // no applicable updates
-		d.Logger.Infoln("no applicable updates")
-		return nil
-	case utils.DSUExitCodeRebootRequired: // updates applied, reboot required
-		d.Logger.Infoln("updates applied, reboot required")
-		d.PendingReboot = true
-		return nil
-	default:
-		return fmt.Errorf("executing dsu returned error: %s, exit code: %d", err.Error(), exitCode)
-	}
-
-}
-
-// pre sets up prequisites for dealing with updates
-func (d *Dell) pre() (err error) {
-
-	errPrefix := "dell dsu prereqs setup error: "
-
-	if d.DsuPrequisitesInstalled {
-		return nil
-	}
-
-	actions := []func() error{
-		d.enableRepo, d.installPkgs, d.startSrvHelper,
-	}
-
-	for _, action := range actions {
-		err := action()
-		if err != nil {
-			return fmt.Errorf(errPrefix + err.Error())
-		}
-	}
-
-	d.Logger.Info("Dell DSU prerequisites setup complete")
-	d.DsuPrequisitesInstalled = true
-
-	return nil
-}
-
-func (d *Dell) installPkgs() error {
-
-	// install dsu package
-	var dsuPkgs []string
-
-	if d.FirmwareUpdateConfig != nil && len(d.FirmwareUpdateConfig.Updates) > 0 {
-		for _, version := range d.FirmwareUpdateConfig.Updates {
-			dsuPkgs = append(dsuPkgs, "dell-system-update-"+version)
-		}
-	} else {
-		dsuPkgs = append(dsuPkgs, "dell-system-update")
-	}
-
-	err := d.Dnf.InstallOneOf(dsuPkgs)
+	// fetch applicable updates and install
+	err = d.fetchAndApplyUpdates(d.DM.Device.ComponentUpdates, config, dryRun)
 	if err != nil {
 		return err
 	}
 
-	// install dsu related tools
-	miscPkgs := []string{
-		"srvadmin-idracadm7",
-		"usbutils",
-		"OpenIPMI",
-		"net-tools",
-	}
-
-	err = d.Dnf.Install(miscPkgs)
-	if err != nil {
-		return fmt.Errorf("Attempts to install dsu related tools: " + err.Error())
-	}
-
 	return nil
 }
-
-func (d *Dell) enableRepo() error {
-
-	// the update environment this dsu package is being installed
-	// environment is one of production, vanguard, canary
-	// the update environment is used by fup to segregate devices under upgrade for testing/production
-	var updateEnv string
-
-	// if a deploy manifest was defined
-	if d.FirmwareUpdateConfig != nil && d.FirmwareUpdateConfig.UpdateEnv != "" {
-		updateEnv = d.FirmwareUpdateConfig.UpdateEnv
-	} else {
-		updateEnv = "production"
-	}
-
-	repos := []string{updateEnv + "-dell-system-update_independent", updateEnv + "-dell-system-update_dependent"}
-
-	return d.Dnf.EnableRepo(repos)
-
-}
-
-// startSrvHelper starts up the service that loads various ipmi modules,
-// Since we're running dsu within a docker container on the target host,
-// this was found to be required to ensure dsu was able to inventorize the host correctly.
-// else it would not be able to retrieve data over IPMI
-func (d *Dell) startSrvHelper() error {
-
-	if os.Getenv("IRONLIB_TEST") != "" {
-		return nil
-	}
-
-	e := utils.NewExecutor("/usr/libexec/instsvcdrv-helper")
-	e.SetArgs([]string{"start"})
-
-	result, err := e.ExecWithContext(context.Background())
-
-	if err != nil || result.ExitCode != 0 {
-		return err
-	}
-
-	return nil
-
-}
-
-//
