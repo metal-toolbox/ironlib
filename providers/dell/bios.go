3f95b054801550f0256c604b474653ad372cfe98
Scott Garman <sgarman@equinix.com>
2021-05-12 15:37:36 -0700

    Check runtime pre-requisites before running racadm commands

--- providers/dell/bios.go
+++ providers/dell/bios.go
@@ -12,6 +12,10 @@ func (d *Dell) SetBIOSConfiguration(ctx context.Context, config *config.BIOSConf
 }
 
 func (d *Dell) GetBIOSConfiguration(ctx context.Context) (*config.BIOSConfiguration, error) {
+	err := d.pre() // ensure runtime pre-requisites are installed
+	if err != nil {
+		return nil, err
+	}
 	racadm := utils.NewDellRacadm(false)
 	return racadm.GetBIOSConfiguration(ctx)
 }
