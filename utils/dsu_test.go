f5e36389f8cead6d28d6c54d644041f74dfeb7d0
Joel Rebello <jrebello@packet.com>
2021-05-20 10:23:47 +0200

    utils/dsu: various fixes and linting
    - add test data

--- utils/dsu_test.go
+++ utils/dsu_test.go
@@ -9,9 +9,9 @@ import (
 )
 
 func Test_dsuParseInventoryBytes(t *testing.T) {
-
 	d := NewFakeDsu()
 	d.Executor.SetArgs([]string{"--import-public-key", "--inventory"})
+
 	result, err := d.Executor.ExecWithContext(context.Background())
 	if err != nil {
 		t.Errorf(err.Error())
@@ -21,13 +21,12 @@ func Test_dsuParseInventoryBytes(t *testing.T) {
 	assert.Equal(t, 18, len(components))
 	assert.Equal(t, "BOSS", components[1].Name)
 	assert.Equal(t, "Boss Adapter", components[1].Slug)
-
 }
 
 func Test_dsuParsePreviewBytes(t *testing.T) {
-
 	d := NewFakeDsu()
 	d.Executor.SetArgs([]string{"--import-public-key", "--preview"})
+
 	result, err := d.Executor.ExecWithContext(context.Background())
 	if err != nil {
 		t.Errorf(err.Error())
@@ -39,11 +38,9 @@ func Test_dsuParsePreviewBytes(t *testing.T) {
 	assert.Equal(t, "SAS HBA330 Controller", components[0].Slug)
 	assert.Equal(t, "16.17.01.00", components[0].FirmwareAvailable)
 	assert.Equal(t, "SAS-Non-RAID_Firmware_124X2_LN_16.17.01.00_A08", components[0].Metadata["firmware_available_filename"])
-
 }
 
 func Test_findDSUInventoryCollector(t *testing.T) {
-
 	invb := "invcol_5N2WM_LN64_20_09_200_921_A00.BIN"
 	dirs := []string{
 		"/tmp" + "/foo/dsu",
@@ -62,13 +59,12 @@ func Test_findDSUInventoryCollector(t *testing.T) {
 
 		f := d + "/" + invb
 		expected = append(expected, f)
+
 		_, err = os.Create(f)
 		if err != nil {
 			t.Error(err)
 		}
-
 	}
 
 	assert.ElementsMatch(t, expected, findDSUInventoryCollector(dirs[0]))
-
 }
