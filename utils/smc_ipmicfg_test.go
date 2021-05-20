453d16c4dea6da1935536798d8303002039a2a62
Joel Rebello <jrebello@packet.com>
2021-05-20 10:20:36 +0200

    utils/smc*: lint, purge component IDs

--- utils/smc_ipmicfg_test.go
+++ utils/smc_ipmicfg_test.go
@@ -9,19 +9,18 @@ import (
 )
 
 func Test_IpmicfgDeviceAttributes(t *testing.T) {
-
 	expected := []*model.Component{
 		{Vendor: "Supermicro", Model: "Supermicro", Name: "CPLD", Slug: "CPLD", FirmwareInstalled: "02.b6.04"},
 		{Vendor: "Supermicro", Model: "Supermicro", Name: "BIOS", Slug: "BIOS", FirmwareInstalled: "3.3"},
 		{Vendor: "Supermicro", Model: "Supermicro", Name: "BMC", Slug: "BMC", FirmwareInstalled: "1.71.11"},
 	}
 	i := NewFakeIpmicfg()
+
 	inventory, err := i.Components()
 	if err != nil {
 		t.Error(err)
 	}
-	// since the component IDs are unique
-	inventory = purgeTestComponentID(inventory)
+
 	assert.Equal(t, expected, inventory)
 }
 
@@ -30,15 +29,13 @@ func Test_IpmicfgParseSummaryOutput(t *testing.T) {
 
 	i := NewFakeIpmicfg()
 	i.Executor.SetArgs([]string{"-summary"})
+
 	result, err := i.Executor.ExecWithContext(context.Background())
 	if err != nil {
 		t.Error(err)
 	}
 
 	summary := i.parseQueryOutput(result.Stdout)
-	if err != nil {
-		t.Error(err)
-	}
 
 	assert.Equal(t, expected, summary)
 }
