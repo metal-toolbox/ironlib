c1e9a47c553f7ed9d64e0a07ef63c4d1e9cb7dea
Joel Rebello <jrebello@packet.com>
2021-05-20 10:18:41 +0200

    utils/nvme: update slugs, purge component IDs, lint

--- utils/nvme_test.go
+++ utils/nvme_test.go
@@ -14,19 +14,17 @@ func newFakeNvme() *Nvme {
 }
 
 func Test_NvmeComponents(t *testing.T) {
-
 	expected := []*model.Component{
-		{Serial: "Z9DF70I8FY3L", Vendor: "TOSHIBA", Model: "KXG60ZNV256G TOSHIBA", FirmwareInstalled: "AGGA4104", Slug: "[0] NVME drive", Name: "NVME drive"},
-		{Serial: "Z9DF70I9FY3L", Vendor: "TOSHIBA", Model: "KXG60ZNV256G TOSHIBA", FirmwareInstalled: "AGGA4104", Slug: "[1] NVME drive", Name: "NVME drive"},
+		{Serial: "Z9DF70I8FY3L", Vendor: "TOSHIBA", Model: "KXG60ZNV256G TOSHIBA", FirmwareInstalled: "AGGA4104", Slug: model.SlugDrive, Name: "NVME drive"},
+		{Serial: "Z9DF70I9FY3L", Vendor: "TOSHIBA", Model: "KXG60ZNV256G TOSHIBA", FirmwareInstalled: "AGGA4104", Slug: model.SlugDrive, Name: "NVME drive"},
 	}
 
 	n := newFakeNvme()
+
 	components, err := n.Components()
 	if err != nil {
 		t.Error(err)
 	}
 
-	// since the component IDs are unique
-	components = purgeTestComponentID(components)
 	assert.Equal(t, expected, components)
 }
