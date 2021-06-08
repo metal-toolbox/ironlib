5a9dbd1263cdf9815a1406c88034614781cfe14a
Joel Rebello <jrebello@packet.com>
2021-06-08 16:10:06 +0200

    lint: linter update requires fix
    the linter update required these files to be fixed
    
    utils/errors.go:14:6: type name will be used as utils.UtilsExecError by
    other packages, and that stutters; consider calling this ExecError
    (golint)
    type UtilsExecError struct {
         ^
    make: *** [lint] Error 1

--- utils/errors.go
+++ utils/errors.go
@@ -10,8 +10,8 @@ var (
 	ErrVersionStrExpectedSemver = errors.New("expected version string to follow semver format")
 )
 
-// UtilsExecError is returned when the command exits with an error or a non zero exit status
-type UtilsExecError struct {
+// ExecError is returned when the command exits with an error or a non zero exit status
+type ExecError struct {
 	Cmd      string
 	Stderr   string
 	Stdout   string
@@ -19,12 +19,12 @@ type UtilsExecError struct {
 }
 
 // Error implements the error interface
-func (u *UtilsExecError) Error() string {
+func (u *ExecError) Error() string {
 	return fmt.Sprintf("cmd %s exited with error: %s\n\t exitCode: %d\n\t stdout: %s", u.Cmd, u.Stderr, u.ExitCode, u.Stdout)
 }
 
-func newUtilsExecError(cmd string, r *Result) *UtilsExecError {
-	return &UtilsExecError{
+func newExecError(cmd string, r *Result) *ExecError {
+	return &ExecError{
 		Cmd:      cmd,
 		Stderr:   string(r.Stderr),
 		Stdout:   string(r.Stdout),
