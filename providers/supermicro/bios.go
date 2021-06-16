32aac7a19513c6368ad6dd2e25ed5bf01781952f
Joel Rebello <jrebello@packet.com>
2021-06-16 17:46:05 +0200

    providers/supermicro: implement method to export BIOS config

--- providers/supermicro/bios.go
+++ providers/supermicro/bios.go
@@ -4,12 +4,23 @@ import (
 	"context"
 
 	"github.com/packethost/ironlib/config"
+	"github.com/packethost/ironlib/utils"
+	"github.com/sirupsen/logrus"
 )
 
+// SetBIOSConfiguration sets bios configuration settings
 func (s *supermicro) SetBIOSConfiguration(ctx context.Context, cfg *config.BIOSConfiguration) error {
 	return nil
 }
 
+// GetBIOSConfiguration returns bios configuration settings
 func (s *supermicro) GetBIOSConfiguration(ctx context.Context) (*config.BIOSConfiguration, error) {
-	return &config.BIOSConfiguration{}, nil
+	var trace bool
+	if s.logger.Level >= logrus.TraceLevel {
+		trace = true
+	}
+
+	sum := utils.NewSupermicroSUM(trace)
+
+	return sum.GetBIOSConfiguration(ctx)
 }
