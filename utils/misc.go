e6f902ab42b408cc3bbebfd1a49d797c88b416dc
Joel Rebello <jrebello@packet.com>
2021-09-13 16:32:22 +0200

    utils: add helper methods and tests

--- utils/misc.go
+++ utils/misc.go
@@ -1,5 +1,13 @@
 package utils
 
+import (
+	"io/ioutil"
+
+	"github.com/packethost/ironlib/errs"
+	"github.com/packethost/ironlib/model"
+	"github.com/pkg/errors"
+)
+
 // StringInSlice returns a bool value if the given string was not/found in the given slice.
 func StringInSlice(str string, sl []string) bool {
 	for _, element := range sl {
@@ -9,4 +17,56 @@ func StringInSlice(str string, sl []string) bool {
 	}
 
 	return false
+}
+
+// copyFile makes a copy of the given file from src to dst
+// setting the default permissions of 0644
+func copyFile(src, dst string) error {
+	in, err := ioutil.ReadFile(src)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(dst, in, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// IdentifyVendorModel returns the device vendor, model, serial number attributes
+func IdentifyVendorModel(dmidecode *Dmidecode) (deviceVendor, deviceModel, deviceSerial string, err error) {
+	deviceVendor, err = dmidecode.Manufacturer()
+	if err != nil {
+		return deviceVendor, deviceModel, deviceSerial, errors.Wrap(errs.NewDmidecodeValueError("manufacturer", ""), err.Error())
+	}
+
+	deviceModel, err = dmidecode.ProductName()
+	if err != nil {
+		return deviceVendor, deviceModel, deviceSerial, errors.Wrap(errs.NewDmidecodeValueError("Product name", ""), err.Error())
+	}
+
+	// identify the vendor from the baseboard manufacturer - if the System Manufacturer attribute is unset
+	if deviceVendor == "" || deviceVendor == model.SystemManufacturerUndefined {
+		deviceVendor, err = dmidecode.BaseBoardManufacturer()
+		if err != nil {
+			return deviceVendor, deviceModel, deviceSerial, errors.Wrap(errs.NewDmidecodeValueError("Baseboard Manufacturer", ""), err.Error())
+		}
+	}
+
+	// identify the model from the baseboard - if the System Product Name is unset
+	if deviceModel == model.SystemManufacturerUndefined {
+		deviceModel, err = dmidecode.BaseBoardProductName()
+		if err != nil {
+			return deviceVendor, deviceModel, deviceSerial, errors.Wrap(errs.NewDmidecodeValueError("Baseboard ProductName", ""), err.Error())
+		}
+	}
+
+	deviceSerial, err = dmidecode.SerialNumber()
+	if err != nil {
+		return deviceVendor, deviceModel, deviceSerial, errors.Wrap(errs.NewDmidecodeValueError("Serial", ""), err.Error())
+	}
+
+	return deviceVendor, deviceModel, deviceSerial, nil
 }
---
+++ utils/misc_test.go
@@ -0,0 +1,64 @@
+package utils
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/packethost/ironlib/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CopyFile(t *testing.T) {
+	srcFile := "/tmp/foobar"
+	dstFile := "/tmp/barfoo"
+	content := []byte(`meh`)
+
+	f, err := os.Create(srcFile)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = f.Write(content)
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer os.Remove(srcFile)
+
+	err = copyFile(srcFile, dstFile)
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer os.Remove(dstFile)
+
+	stat, err := os.Stat(dstFile)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, "-rw-r--r--", stat.Mode().String())
+
+	b, err := os.ReadFile(dstFile)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, content, b)
+}
+
+func Test_IdentifyVendorModel(t *testing.T) {
+	dmi, err := InitTestDmidecode("../fixtures/asrr/e3c246d4i-nl/dmidecode-non-packet")
+	if err != nil {
+		t.Error(err)
+	}
+
+	v, m, _, err := IdentifyVendorModel(dmi)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, model.VendorAsrockrack, strings.ToLower(v))
+	assert.Equal(t, "E3C246D4I-NL", m)
+}
