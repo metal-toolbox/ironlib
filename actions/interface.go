c88f7e26e6b0496a6c9688dd159106411bc0b192
Joel Rebello <jrebello@packet.com>
2022-11-25 15:44:17 +0100

    actions; define Utils attribute getter interface and inherit in configurors, collectors, updaters.
    This interface defines a method to retrieve the utility attributes
    like the name, absolute path and an error if it was not found on the
    host system.

--- actions/interface.go
+++ actions/interface.go
@@ -21,18 +21,26 @@ type BIOSConfiguror interface {
 	GetBIOSConfiguration(ctx context.Context, deviceModel string) (map[string]string, error)
 }
 
+// UtilAttributeGetter defines methods to retrieve utility attributes.
+type UtilAttributeGetter interface {
+	Attributes() (utilName string, absolutePath string, err error)
+}
+
 // Updater defines an interface to install an update file
 type Updater interface {
+	UtilAttributeGetter
 	ApplyUpdate(ctx context.Context, updateFile, component string) error
 }
 
 // InventoryCollector defines an interface to collect all device inventory
 type InventoryCollector interface {
+	UtilAttributeGetter
 	Collect(ctx context.Context, device *common.Device) error
 }
 
 // DriveCollector defines an interface to return disk drive inventory
 type DriveCollector interface {
+	UtilAttributeGetter
 	Drives(ctx context.Context) ([]*common.Drive, error)
 }
 
@@ -40,36 +48,43 @@ type DriveCollector interface {
 //
 // The logicalName is the kernel/OS assigned drive name - /dev/sdX or /dev/nvmeX
 type DriveCapabilityCollector interface {
+	UtilAttributeGetter
 	DriveCapabilities(ctx context.Context, logicalName string) ([]*common.Capability, error)
 }
 
 // NICCollector defines an interface to returns NIC inventory
 type NICCollector interface {
+	UtilAttributeGetter
 	NICs(ctx context.Context) ([]*common.NIC, error)
 }
 
 // BMCCollector defines an interface to return BMC inventory
 type BMCCollector interface {
+	UtilAttributeGetter
 	BMC(ctx context.Context) (*common.BMC, error)
 }
 
 // CPLDCollector defines an interface to return CPLD inventory
 type CPLDCollector interface {
+	UtilAttributeGetter
 	CPLDs(ctx context.Context) ([]*common.CPLD, error)
 }
 
 // BIOSCollector defines an interface to return BIOS inventory
 type BIOSCollector interface {
+	UtilAttributeGetter
 	BIOS(ctx context.Context) (*common.BIOS, error)
 }
 
 // StorageControllerCollector defines an interface to returns storage controllers inventory
 type StorageControllerCollector interface {
+	UtilAttributeGetter
 	StorageControllers(ctx context.Context) ([]*common.StorageController, error)
 }
 
 // TPMCollector defines an interface to collect TPM device inventory
 type TPMCollector interface {
+	UtilAttributeGetter
 	TPMs(ctx context.Context) ([]*common.TPM, error)
 }
 
@@ -77,31 +92,37 @@ type TPMCollector interface {
 
 // DriveUpdater defines an interface to update drive firmware
 type DriveUpdater interface {
+	UtilAttributeGetter
 	UpdateDrive(ctx context.Context, updateFile, modelNumber, serialNumber string) error
 }
 
 // NICUpdater defines an interface to update NIC firmware
 type NICUpdater interface {
+	UtilAttributeGetter
 	UpdateNIC(ctx context.Context, updateFile, modelNumber string) error
 }
 
 // BMCUpdater defines an interface to update BMC firmware
 type BMCUpdater interface {
+	UtilAttributeGetter
 	UpdateBMC(ctx context.Context, updateFile, modelNumber string) error
 }
 
 // CPLDUpdater defines an interface to update CPLD firmware
 type CPLDUpdater interface {
+	UtilAttributeGetter
 	UpdateCPLD() error
 }
 
 // BIOSUpdater defines an interface to update BIOS firmware
 type BIOSUpdater interface {
+	UtilAttributeGetter
 	UpdateBIOS(ctx context.Context, updateFile, modelNumber string) error
 }
 
 // StorageControllerUpdater defines an interface to update storage controller firmware
 type StorageControllerUpdater interface {
+	UtilAttributeGetter
 	UpdateStorageController() error
 }
 
