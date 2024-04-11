0f547b7a09961ba09df848baa3f7d98aa962d1b6
Manuel Mendez <github@i.m.mmlb.dev>
2024-04-11 18:38:55 -0400

    utils/nvme: Parse nvme json output not human readable
    JSON + bit twiddling is more likely to be future compatible (hopefully).
    This library is not a human and thus should not use the human readable
    output. I would say that json + bit fiddling is also easier to
    understand than a regex based state machine.

--- utils/nvme.go
+++ utils/nvme.go
@@ -1,11 +1,9 @@
 package utils
 
 import (
-	"bufio"
 	"context"
 	"encoding/json"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 
@@ -86,7 +84,6 @@ func (n *Nvme) Drives(ctx context.Context) ([]*common.Drive, error) {
 		if len(modelTokens) > 1 {
 			vendor = modelTokens[1]
 		}
-
 		drive := &common.Drive{
 			Common: common.Common{
 				Serial:      d.SerialNumber,
@@ -130,9 +127,8 @@ func (n *Nvme) list(ctx context.Context) ([]byte, error) {
 }
 
 func (n *Nvme) cmdListCapabilities(ctx context.Context, logicalPath string) ([]byte, error) {
-	// nvme id-ctrl -H devicepath
-	n.Executor.SetArgs([]string{"id-ctrl", "-H", logicalPath})
-
+	// nvme id-ctrl --output-format=json devicepath
+	n.Executor.SetArgs([]string{"id-ctrl", "--output-format=json", logicalPath})
 	result, err := n.Executor.ExecWithContext(ctx)
 	if err != nil {
 		return nil, err
@@ -141,161 +137,195 @@ func (n *Nvme) cmdListCapabilities(ctx context.Context, logicalPath string) ([]b
 	return result.Stdout, nil
 }
 
-// DriveCapabilities returns the drive capability attributes obtained through hdparm
+// DriveCapabilities returns the drive capability attributes obtained through nvme
 //
 // The logicalName is the kernel/OS assigned drive name - /dev/nvmeX
 //
 // This method implements the actions.DriveCapabilityCollector interface.
-//
-// nolint:gocyclo // line parsing is cyclomatic
 func (n *Nvme) DriveCapabilities(ctx context.Context, logicalName string) ([]*common.Capability, error) {
 	out, err := n.cmdListCapabilities(ctx, logicalName)
 	if err != nil {
 		return nil, err
 	}
 
-	var capabilitiesFound []*common.Capability
-
-	var lines []string
-
-	s := string(out)
-
-	scanner := bufio.NewScanner(strings.NewReader(s))
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+	var caps struct {
+		FNA     uint `json:"fna"`
+		SANICAP uint `json:"sanicap"`
 	}
 
-	err = scanner.Err()
-	if err != nil {
+	if err := json.Unmarshal(out, &caps); err != nil {
 		return nil, err
 	}
 
-	// Delimiters
-	reFnaStart := regexp.MustCompile(`(?s)^fna\s`)
-	reFnaEnd := regexp.MustCompile(`(?s)^vwc\s`)
-	reSaniStart := regexp.MustCompile(`(?s)^sanicap\s`)
-	reSaniEnd := regexp.MustCompile(`(?s)^hmminds\s`)
-	reBlank := regexp.MustCompile(`(?m)^\s*$`)
+	var capabilitiesFound []*common.Capability
+	capabilitiesFound = append(capabilitiesFound, parseSanicap(caps.SANICAP)...)
+	capabilitiesFound = append(capabilitiesFound, parseFna(caps.FNA)...)
 
-	var fnaBool, saniBool bool
+	return capabilitiesFound, nil
+}
 
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		fnaStart := reFnaStart.MatchString(line)
-		fnaEnd := reFnaEnd.MatchString(line)
-		saniStart := reSaniStart.MatchString(line)
-		saniEnd := reSaniEnd.MatchString(line)
-		isBlank := reBlank.MatchString(line)
+func parseFna(fna uint) []*common.Capability {
+	// Bit masks values came from nvme-cli repo:
+	// https://github.com/linux-nvme/nvme-cli/blob/v2.8/nvme-print-stdout.c#L2199-L2217
 
-		// start/end match specific block delimiters
-		// bools are toggled to indicate lines within a given block
-		switch {
-		case fnaStart:
-			fnaBool = true
-		case fnaEnd:
-			fnaBool = false
-		case saniStart:
-			saniBool = true
-		case saniEnd:
-			saniBool = false
-		}
+	caps := make([]*common.Capability, 0, 4)
 
-		switch {
-		case (fnaStart || saniStart):
-			capability := new(common.Capability)
+	caps = append(caps, &common.Capability{
+		Name:        "fna",
+		Description: "Crypto Erase Support",
+		Enabled:     fna != 0,
+	})
 
-			partsLen := 2
-
-			parts := strings.Split(line, ":")
-			if len(parts) != partsLen {
-				continue
-			}
-
-			key, value := strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])
-			capability.Name = key
-
-			if value != "0" {
-				capability.Enabled = true
-			}
-
-			if fnaStart {
-				capability.Description = "Crypto Erase Support"
-			} else {
-				capability.Description = "Sanitize Support"
-			}
-
-			if value != "0" {
-				capability.Enabled = true
-			}
-
-			capabilitiesFound = append(capabilitiesFound, capability)
-
-			// crypto erase
-		case (fnaBool && !fnaEnd && !isBlank):
-			capability := new(common.Capability)
-
-			partsLen := 3
-
-			parts := strings.Split(line, ":")
-			if len(parts) != partsLen {
-				continue
-			}
-
-			data := strings.Split(parts[2], "\t")
-			enabled := strings.TrimSpace(data[0])
-
-			if enabled != "0" {
-				capability.Enabled = true
-			}
-
-			// Generate short flag identifier
-			for _, word := range strings.Fields(data[1]) {
-				capability.Name += strings.ToLower(word[0:1])
-			}
-
-			capability.Description = data[1]
-
-			if enabled != "0" {
-				capability.Enabled = true
-			}
-
-			capability.Description = data[1]
-			capabilitiesFound = append(capabilitiesFound, capability)
-			// sanitize
-		case (saniBool && !saniEnd && !isBlank):
-			capability := new(common.Capability)
-
-			partsLen := 3
-
-			parts := strings.Split(line, ":")
-			if len(parts) != partsLen {
-				continue
-			}
-
-			data := strings.Split(parts[2], "\t")
-			enabled := strings.TrimSpace(data[0])
-
-			if enabled != "0" {
-				capability.Enabled = true
-			}
-
-			// Generate short flag identifier
-			for _, word := range strings.Fields(data[1]) {
-				capability.Name += strings.ToLower(word[0:1])
-			}
-
-			capability.Description = data[1]
-
-			if enabled != "0" {
-				capability.Enabled = true
-			}
-
-			capability.Description = data[1]
-			capabilitiesFound = append(capabilitiesFound, capability)
-		}
+	if (fna&(0b1<<2))>>2 == 0 {
+		caps = append(caps, &common.Capability{
+			Name:        "censapose",
+			Description: "Crypto Erase Not Supported as part of Secure Erase",
+			Enabled:     false,
+		})
+	} else {
+		caps = append(caps, &common.Capability{
+			Name:        "cesapose",
+			Description: "Crypto Erase Supported as part of Secure Erase",
+			Enabled:     true,
+		})
 	}
 
-	return capabilitiesFound, err
+	// nvme: cens
+	if (fna&(0b1<<1))>>1 == 0 {
+		caps = append(caps, &common.Capability{
+			Name:        "ceatsn",
+			Description: "Crypto Erase Applies to Single Namespace(s)",
+			Enabled:     false,
+		})
+	} else {
+		caps = append(caps, &common.Capability{
+			Name:        "ceatan",
+			Description: "Crypto Erase Applies to All Namespace(s)",
+			Enabled:     true,
+		})
+	}
+
+	// nvme: fmns
+	if (fna&(0b1<<0))>>0 == 0 {
+		caps = append(caps, &common.Capability{
+			Name:        "fatsn",
+			Description: "Format Applies to Single Namespace(s)",
+			Enabled:     false,
+		})
+	} else {
+		caps = append(caps, &common.Capability{
+			Name:        "fatan",
+			Description: "Format Applies to All Namespace(s)",
+			Enabled:     true,
+		})
+	}
+
+	return caps
+}
+
+func parseSanicap(sanicap uint) []*common.Capability {
+	// Bit masks values came from nvme-cli repo:
+	// https://github.com/linux-nvme/nvme-cli/blob/v2.8/nvme-print-stdout.c#L2064-L2093
+
+	caps := make([]*common.Capability, 0, 6)
+
+	caps = append(caps, &common.Capability{
+		Name:        "sanicap",
+		Description: "Sanitize Support",
+		Enabled:     sanicap != 0,
+	})
+
+	// nvme: nodmmas
+	switch (sanicap & (0b11 << 30)) >> 30 {
+	case 0b00:
+		caps = append(caps, &common.Capability{
+			Name:        "ammasocsind",
+			Description: "Additional media modification after sanitize operation completes successfully is not defined",
+			Enabled:     false,
+		})
+	case 0b01:
+		caps = append(caps, &common.Capability{
+			Name:        "minamasocs",
+			Description: "Media is not additionally modified after sanitize operation completes successfully",
+			Enabled:     true,
+		})
+	case 0b10:
+		caps = append(caps, &common.Capability{
+			Name:        "miamasocs",
+			Description: "Media is additionally modified after sanitize operation completes successfully",
+			Enabled:     true,
+		})
+	case 0b11:
+		caps = append(caps, &common.Capability{
+			Name:        "",
+			Description: "Reserved",
+			Enabled:     true,
+		})
+	default:
+		panic("unreachable")
+	}
+
+	// nvme: ndi
+	if (sanicap&(0b1<<29))>>29 == 0 {
+		caps = append(caps, &common.Capability{
+			Name:        "nasbiscs",
+			Description: "No-Deallocate After Sanitize bit in Sanitize command Supported",
+			Enabled:     false,
+		})
+	} else {
+		caps = append(caps, &common.Capability{
+			Name:        "nasbiscns",
+			Description: "No-Deallocate After Sanitize bit in Sanitize command Not Supported",
+			Enabled:     true,
+		})
+	}
+
+	// nvme: owr
+	if (sanicap&(0b1<<2))>>2 == 0 {
+		caps = append(caps, &common.Capability{
+			Name:        "osons",
+			Description: "Overwrite Sanitize Operation Not Supported",
+			Enabled:     false,
+		})
+	} else {
+		caps = append(caps, &common.Capability{
+			Name:        "osos",
+			Description: "Overwrite Sanitize Operation Supported",
+			Enabled:     true,
+		})
+	}
+
+	// nvme: ber
+	if (sanicap&(0b1<<1))>>1 == 0 {
+		caps = append(caps, &common.Capability{
+			Name:        "besons",
+			Description: "Block Erase Sanitize Operation Not Supported",
+			Enabled:     false,
+		})
+	} else {
+		caps = append(caps, &common.Capability{
+			Name:        "besos",
+			Description: "Block Erase Sanitize Operation Supported",
+			Enabled:     true,
+		})
+	}
+
+	// nvme: cer
+	if (sanicap&(0b1<<0))>>0 == 0 {
+		caps = append(caps, &common.Capability{
+			Name:        "cesons",
+			Description: "Crypto Erase Sanitize Operation Not Supported",
+			Enabled:     false,
+		})
+	} else {
+		caps = append(caps, &common.Capability{
+			Name:        "cesos",
+			Description: "Crypto Erase Sanitize Operation Supported",
+			Enabled:     true,
+		})
+	}
+
+	return caps
 }
 
 // NewFakeNvme returns a mock nvme collector that returns mock data for use in tests.
