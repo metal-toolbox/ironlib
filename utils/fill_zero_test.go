4ebae7d37fea961457603b46e0c09b13215dc23f
Ignacio Turégano <ignacio.turegano@eu.equinix.com>
2024-04-19 15:42:01 +0200

    Testing against a range of file sizes (also a non-existent file)

--- utils/fill_zero_test.go
+++ utils/fill_zero_test.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"context"
 	"os"
+	"strconv"
 	"testing"
 )
 
@@ -14,74 +15,38 @@ func Test_NewFillZeroCmd(t *testing.T) {
 	}
 }
 
-func Test_Fill(t *testing.T) {
-	// Create a temporary file for testing
-	tmpfile, err := os.CreateTemp("", "example")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(tmpfile.Name()) // clean up
-
-	// Write some content to the temporary file
-	expectedSize := int64(4096)
-	if _, err = tmpfile.Write(make([]byte, expectedSize)); err != nil {
-		t.Fatal(err)
-	}
-
-	// Simulate a context
-	ctx := context.Background()
-
-	// Create a FillZero instance
-	zw := &FillZero{}
-
-	// Test Fill function
-	err = zw.Fill(ctx, tmpfile.Name())
-	if err != nil {
-		t.Errorf("Fill returned an error: %v", err)
-	}
-
-	// Check if the file size remains the same after overwrite
-	fileInfo, err := os.Stat(tmpfile.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-	if size := fileInfo.Size(); size != expectedSize {
-		t.Errorf("Expected file size to remain %d after overwrite, got %d", expectedSize, size)
-	}
-}
-
 func Test_WipeDisk(t *testing.T) {
-	// Create a temporary file for testing
-	tmpfile, err := os.CreateTemp("", "example")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(tmpfile.Name()) // clean up
 
-	// Write some content to the temporary file
-	expectedSize := int64(4096)
-	if _, err = tmpfile.Write(make([]byte, expectedSize)); err != nil {
-		t.Fatal(err)
-	}
-
-	// Simulate a context
-	ctx := context.Background()
-
-	// Create a FillZero instance
-	zw := &FillZero{}
-
-	// Test FillDisk function
-	err = zw.WipeDisk(ctx, tmpfile.Name())
-	if err != nil {
-		t.Errorf("FillDisk returned an error: %v", err)
-	}
-
-	// Check if the file size remains the same after overwrite
-	fileInfo, err := os.Stat(tmpfile.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-	if size := fileInfo.Size(); size != expectedSize {
-		t.Errorf("Expected file size to remain %d after overwrite, got %d", expectedSize, size)
+	for _, size := range []int{4095, 4096, 4097, 8192} {
+		t.Run(strconv.Itoa(size), func(t *testing.T) {
+			// Create a temporary file for testing
+			tmpfile, err := os.CreateTemp("", "example")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.Remove(tmpfile.Name()) // clean up
+			// Write some content to the temporary file
+			expectedSize := int64(4096)
+			if _, err = tmpfile.Write(make([]byte, expectedSize)); err != nil {
+				t.Fatal(err)
+			}
+			// Simulate a context
+			ctx := context.Background()
+			// Create a FillZero instance
+			zw := &FillZero{}
+			// Test Fill function
+			err = zw.WipeDisk(ctx, tmpfile.Name())
+			if err != nil {
+				t.Errorf("Fill returned an error: %v", err)
+			}
+			// Check if the file size remains the same after overwrite
+			fileInfo, err := os.Stat(tmpfile.Name())
+			if err != nil {
+				t.Fatal(err)
+			}
+			if size := fileInfo.Size(); size != expectedSize {
+				t.Errorf("Expected file size to remain %d after overwrite, got %d", expectedSize, size)
+			}
+		})
 	}
 }
