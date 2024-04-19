a0101580ddb2751d6f83fa06db7714d6b4437ac9
Ignacio Turégano <ignacio.turegano@eu.equinix.com>
2024-04-19 17:34:27 +0200

    Removed the verbose err and reduced the printProgress to just the print data

--- utils/fill_zero.go
+++ utils/fill_zero.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -26,14 +25,12 @@ func (z *FillZero) WipeDisk(ctx context.Context, path string) error {
 	// Write open
 	file, err := os.OpenFile(path, os.O_WRONLY, 0)
 	if err != nil {
-		log.Println(fmt.Errorf("%w", err))
 		return err
 	}
 	defer file.Close()
 	// Get disk or partition size
 	partitionSize, err := file.Seek(0, io.SeekEnd)
 	if err != nil {
-		log.Println(err)
 		return err
 	}
 	log.Printf("%s | Size: %dB\n", path, partitionSize)
@@ -56,7 +53,9 @@ func (z *FillZero) WipeDisk(ctx context.Context, path string) error {
 		bytesRemaining -= int64(bytesWritten)
 		// Print progress report every 10 seconds and when done
 		if bytesRemaining == 0 || time.Since(start) >= 10*time.Second {
-			printProgress(totalBytesWritten, partitionSize, &start, &bytesSinceLastPrint, bytesRemaining, path)
+			printProgress(totalBytesWritten, partitionSize, &start, &bytesSinceLastPrint, path)
+			start = time.Now()
+			bytesSinceLastPrint = 0
 		}
 	}
 	err = file.Sync()
@@ -66,7 +65,7 @@ func (z *FillZero) WipeDisk(ctx context.Context, path string) error {
 	return nil
 }
 
-func printProgress(totalBytesWritten, partitionSize int64, start *time.Time, bytesSinceLastPrint *int64, bytesRemaining int64, path string) {
+func printProgress(totalBytesWritten, partitionSize int64, start *time.Time, bytesSinceLastPrint *int64, path string) {
 	// Calculate progress and ETA
 	progress := float64(totalBytesWritten) / float64(partitionSize) * 100
 	elapsed := time.Since(*start).Seconds()
@@ -75,8 +74,6 @@ func printProgress(totalBytesWritten, partitionSize int64, start *time.Time, byt
 	remainingHours := float64(remainingSeconds / 3600)
 	mbPerSecond := speed / (1024 * 1024)
 	log.Printf("%s | Progress: %.2f%% | Speed: %.2f MB/s | Estimated time left: %.2f hour(s)\n", path, progress, mbPerSecond, remainingHours)
-	*start = time.Now()
-	*bytesSinceLastPrint = 0
 }
 
 // We are in go 1.19 min not available yet
