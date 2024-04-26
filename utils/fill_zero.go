210552307962fa4a027c4946e362864207184a22
Manuel Mendez <github@i.m.mmlb.dev>
2024-04-26 14:14:53 -0400

    actions/interface: Add logger to WipeDisk params
    We want to be able to log/print progress in WipeDisk since disk wiping
    may take a long time. Instead of using the global logger from log
    package and assuming thats the right thing to do we should take it in as
    a parameter.
    
    This also gets rid of log messages printed during tests while running
    either from within utils directory or by running with -v:
    
    Before:
    ```
    go test
    2024/04/26 14:47:34 Starting zero-fill of /run/user/1000/tmp/example1832453771
    2024/04/26 14:47:34 /run/user/1000/tmp/example1832453771 | Size: 4096B
    2024/04/26 14:47:34 /run/user/1000/tmp/example1832453771 | Progress: 100.00% | Speed: 1676.50 MB/s | Estimated time left: 0.00 hour(s)
    2024/04/26 14:47:34 Starting zero-fill of /run/user/1000/tmp/example1376210307
    2024/04/26 14:47:34 /run/user/1000/tmp/example1376210307 | Size: 4096B
    2024/04/26 14:47:34 /run/user/1000/tmp/example1376210307 | Progress: 100.00% | Speed: 3030.45 MB/s | Estimated time left: 0.00 hour(s)
    2024/04/26 14:47:34 Starting zero-fill of /run/user/1000/tmp/example3643202665
    2024/04/26 14:47:34 /run/user/1000/tmp/example3643202665 | Size: 4096B
    2024/04/26 14:47:34 /run/user/1000/tmp/example3643202665 | Progress: 100.00% | Speed: 3004.81 MB/s | Estimated time left: 0.00 hour(s)
    2024/04/26 14:47:34 Starting zero-fill of /run/user/1000/tmp/example2153188305
    2024/04/26 14:47:34 /run/user/1000/tmp/example2153188305 | Size: 4096B
    2024/04/26 14:47:34 /run/user/1000/tmp/example2153188305 | Progress: 100.00% | Speed: 2981.87 MB/s | Estimated time left: 0.00 hour(s)
    PASS
    ok  	github.com/metal-toolbox/ironlib/utils	0.100s
    ```
    
    After:
    ```
    go test
    PASS
    ok  	github.com/metal-toolbox/ironlib/utils	0.100s
    ```

--- utils/fill_zero.go
+++ utils/fill_zero.go
@@ -2,10 +2,12 @@ package utils
 
 import (
 	"context"
+	"fmt"
 	"io"
-	"log"
 	"os"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 type FillZero struct {
@@ -21,24 +23,29 @@ func NewFillZeroCmd(trace bool) *FillZero {
 	return &z
 }
 
-func (z *FillZero) WipeDisk(ctx context.Context, logicalName string) error {
-	log.Println("Starting zero-fill of", logicalName)
+func (z *FillZero) WipeDisk(ctx context.Context, l *logrus.Logger, logicalName string) error {
+	log := l.WithField("device", logicalName)
+	log.Info("starting zero-fill")
+
 	// Write open
 	file, err := os.OpenFile(logicalName, os.O_WRONLY, 0)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
+
 	// Get disk or partition size
 	partitionSize, err := file.Seek(0, io.SeekEnd)
 	if err != nil {
 		return err
 	}
-	log.Printf("%s | Size: %dB\n", logicalName, partitionSize)
+
+	log.WithField("size", fmt.Sprintf("%dB", partitionSize)).Info("disk info detected")
 	_, err = file.Seek(0, io.SeekStart)
 	if err != nil {
 		return err
 	}
+
 	var bytesSinceLastPrint int64
 	var totalBytesWritten int64
 	buffer := make([]byte, 4096)
@@ -47,7 +54,7 @@ func (z *FillZero) WipeDisk(ctx context.Context, logicalName string) error {
 		// Check if the context has been canceled
 		select {
 		case <-ctx.Done():
-			log.Println("Context canceled. Stopping WipeDisk")
+			log.Info("stopping")
 			return ctx.Err()
 		default:
 			l := min(int64(len(buffer)), bytesRemaining)
@@ -55,12 +62,13 @@ func (z *FillZero) WipeDisk(ctx context.Context, logicalName string) error {
 			if writeError != nil {
 				return writeError
 			}
+
 			totalBytesWritten += int64(bytesWritten)
 			bytesSinceLastPrint += int64(bytesWritten)
 			bytesRemaining -= int64(bytesWritten)
 			// Print progress report every 10 seconds and when done
 			if bytesRemaining == 0 || time.Since(start) >= 10*time.Second {
-				printProgress(totalBytesWritten, partitionSize, &start, &bytesSinceLastPrint, logicalName)
+				printProgress(log, totalBytesWritten, partitionSize, start, bytesSinceLastPrint)
 				start = time.Now()
 				bytesSinceLastPrint = 0
 			}
@@ -73,15 +81,19 @@ func (z *FillZero) WipeDisk(ctx context.Context, logicalName string) error {
 	return nil
 }
 
-func printProgress(totalBytesWritten, partitionSize int64, start *time.Time, bytesSinceLastPrint *int64, path string) {
+func printProgress(log *logrus.Entry, totalBytesWritten, partitionSize int64, start time.Time, bytesSinceLastPrint int64) {
 	// Calculate progress and ETA
 	progress := float64(totalBytesWritten) / float64(partitionSize) * 100
-	elapsed := time.Since(*start).Seconds()
-	speed := float64(*bytesSinceLastPrint) / elapsed                                  // Speed in bytes per second
+	elapsed := time.Since(start).Seconds()
+	speed := float64(bytesSinceLastPrint) / elapsed                                   // Speed in bytes per second
 	remainingSeconds := (float64(partitionSize) - float64(totalBytesWritten)) / speed // Remaining time in seconds
 	remainingHours := float64(remainingSeconds / 3600)
 	mbPerSecond := speed / (1024 * 1024)
-	log.Printf("%s | Progress: %.2f%% | Speed: %.2f MB/s | Estimated time left: %.2f hour(s)\n", path, progress, mbPerSecond, remainingHours)
+	log.WithFields(logrus.Fields{
+		"progress":  fmt.Sprintf("%.2f%%", progress),
+		"speed":     fmt.Sprintf("%.2f MB/s", mbPerSecond),
+		"remaining": fmt.Sprintf("%.2f hour(s)", remainingHours),
+	}).Info("")
 }
 
 // We are in go 1.19 min not available yet
